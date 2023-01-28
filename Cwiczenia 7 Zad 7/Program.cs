b3739143e55f7e2fed919f9e068bc50f5810eef2
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2023-01-28 17:30:20 +0100

    Add files via upload

---
+++ Cwiczenia 7 Zad 7/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Cwiczenia_7_Zad_7
+{
+    static class FunkcjePrzeciazone
+    {
+        static public double Dzielenie(int a, int b)
+        {
+            return a / b;
+        }
+        static public double Dzielenie(double a, double b)
+        {
+            return a / b;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FunkcjePrzeciazone.Dzielenie(12, 5));
+            Console.WriteLine(FunkcjePrzeciazone.Dzielenie(12.0, 5.0));
+        }
+    }
+}
